1f5d689768f77bb9371490d063f72493a3538c4b
liwei <liwei@uama.com.cn>
2017-12-27 15:35:13 +0800

    新增 PureBridgeWebViewClient 做测试

---
+++ uama_webview/src/main/java/com/uama/weight/uama_webview/PureBridgeWebViewClient.java
@@ -0,0 +1,73 @@
+package com.uama.weight.uama_webview;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
+
+import com.tencent.smtt.export.external.interfaces.WebResourceError;
+import com.tencent.smtt.export.external.interfaces.WebResourceRequest;
+import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by bruce on 10/28/15.
+ */
+public class PureBridgeWebViewClient extends WebViewClient {
+    private BridgeWebView webView;
+
+    public PureBridgeWebViewClient(BridgeWebView webView) {
+        this.webView = webView;
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        try {
+            url = URLDecoder.decode(url, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        if (url.startsWith(BridgeUtil.YY_RETURN_DATA)) { // 如果是返回数据
+            webView.handlerReturnData(url);
+            return true;
+        } else if (url.startsWith(BridgeUtil.YY_OVERRIDE_SCHEMA)) { //
+            webView.flushMessageQueue();
+            return true;
+        } else {
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    }
+
+    @Override
+    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        super.onPageStarted(view, url, favicon);
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+
+        if (BridgeWebView.toLoadJs != null) {
+            BridgeUtil.webViewLoadLocalJs(view, BridgeWebView.toLoadJs);
+        }
+
+        //
+        if (webView.getStartupMessage() != null) {
+            for (Message m : webView.getStartupMessage()) {
+                webView.dispatchMessage(m);
+            }
+            webView.setStartupMessage(null);
+        }
+    }
+}
